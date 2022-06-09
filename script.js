61d95a86f2ac30ea561b3ef99000737c8bfe5029
MD. Ariful Islam <61628803+ArifulSUST49@users.noreply.github.com>
2022-06-09 22:42:16 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const navHamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-
-navHamburger.addEventListener("click", () => {
-  navHamburger.classList.toggle("open");
-  navMenu.classList.toggle("open");
-  document.body.classList.toggle("stop-scrolling");
-});
